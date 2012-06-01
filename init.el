ba255f25572069001f1b0e455b47d8452dc577f9
Curt Micol <asenchi@asenchi.com>
2012-06-01 18:09:38 -0400

    A couple more things

--- init.el
+++ init.el
@@ -60,6 +60,18 @@
 (setq exec-path (cons "~/.rbenv/shims" exec-path))
 (setenv "PATH" (concat "~/.rbenv/shims:" (getenv "PATH")))
 
+(defvar user-temporary-file-directory
+  (concat temporary-file-directory user-login-name "/"))
+(make-directory user-temporary-file-directory t)
+(setq backup-by-copying t)
+(setq backup-directory-alist
+      `(("." . ,user-temporary-file-directory)
+        (,tramp-file-name-regexp nil)))
+(setq auto-save-list-file-prefix
+      (concat user-temporary-file-directory ".auto-saves-"))
+(setq auto-save-file-name-transforms
+      `((".*" ,user-temporary-file-directory t)))
+
 ;; Use zsh for shell
 (setq shell-file-name "/bin/zsh")
 
